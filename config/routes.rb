7fb7c19c97d19e5437733c3aff9a9cdf729cfab5
victor goutay <victor.goutay@gmail.com>
2015-11-28 22:12:19 +0100

    move to deleted branch

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'services#index'
   resources :users
+  resources :centres
   resources :services
   resources :bookings
   # The priority is based upon order of creation: first created -> highest priority.
