c6028639c903210e2127aedac343e54bf857cc07
victor goutay <victor.goutay@gmail.com>
2015-12-03 10:45:40 +0100

    switch to master

--- Gemfile
+++ Gemfile
@@ -4,9 +4,12 @@ gem 'masonry-rails'
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'pry'
+# Up load image
 gem 'carrierwave'
+# resize image
 gem 'mini_magick'
 gem 'fog'
+# ------------------
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
--- app/models/centre.rb
+++ app/models/centre.rb
@@ -4,6 +4,6 @@ class Centre < ActiveRecord::Base
   has_many :bookings
   geocoded_by :address
   after_validation :geocode
+  # mount_uploader methods take two parameters to link field in data base and Class in  picture_uploader.rb
   mount_uploader :picture, PictureUploader
-  # mount_uploader :avatar, AvatarUploader
 end
