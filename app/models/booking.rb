556a0f0e30e02ac910de2aad10cce9aed0bacd72
ludivinecp <coipel.ludivine@gmail.com>
2015-12-08 18:02:00 +0100

    show bookings ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,7 @@ class Booking < ActiveRecord::Base
   belongs_to :rider
   validates_presence_of :number_of_people
 
-  # def total_price
-  #   self.number_of_people * service.price    
+  # def total_amount (number_of_people)
+  # self.number_of_people * service.price   
   # end
 end
