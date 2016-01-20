9d5d12fc890dcd455403b30b8a44e7b3d84b7e14
knopfler81 <knopfler42@yahoo.fr>
2016-01-20 15:24:32 +0100

    switched account

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
 
   #Add your ID and secret here
   #ID first, secret second
-  config.omniauth :facebook, "182575595424095", "bf226afdbb4554c6011127f4d6d426eb"
+  config.omniauth :facebook, "954436891314304", "538500e590bb36c74d9b7db86bb378de"
                   #callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # The secret key used by Devise. Devise uses this key to generate
