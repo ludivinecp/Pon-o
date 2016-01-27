5d38c110ac561c7c74d9552c04382927927e4335
ludivinecp <coipel.ludivine@gmail.com>
2016-01-27 20:04:17 +0100

    few modifications for final presentation

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,8 +4,8 @@
 # }
 
 Rails.configuration.stripe = {
-  :publishable_key => "pk_test_YsTyE4WcgRlU1G8HIBEcVqV6",
-  :secret_key      => "sk_test_6sfvcWIUkeqdlcQVBt8bcpKl"
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
